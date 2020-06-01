83b36c9b0eeb5cc634650f7eb69c9d09ce7d8ef8
Roberto Pinho <robertoopinho@gmail.com>
2020-06-01 09:59:46 -0300

    :bug: Bug no heroku

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class config:
-    #SQLALCHEMY_DATABASE_URI = 'mysql://robertoopinho:bancodedados@robertoopinho.mysql.pythonanywhere-services.com/robertoopinho$default'
+    SQLALCHEMY_DATABASE_URI = 'mysql://robertoopinho:bancodedados@robertoopinho.mysql.pythonanywhere-services.com/robertoopinho$default'
     #SQLALCHEMY_DATABASE_URI = 'mysql:// USUARIO : SENHAMYSQL @robertoopinho.mysql.pythonanywhere-services.com/ BANCODEDADOS'
 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/revenda'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/revenda'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SALT = "X#3jfk$%kKmGw&*jKLiPW@!jm345"
     JWT_SECRET_KEY = 'hjsdfhj#$@DFhsms@ldkPç()H#Dnx3@'
